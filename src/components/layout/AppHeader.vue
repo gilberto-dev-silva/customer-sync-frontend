d472d48fbcac10f600a115d10c8b9cab5a296e11
Gilberto Silva <gilbertdsbatista@gmail.com>
2025-05-05 08:13:35 -0300

    feat: create AppHeader component with dynamic title support

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="app-header-main bg-primary text-white shadow-sm">
+    <div class="conatiner-header-main">
+      <h1>{{ props.title }}</h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+interface TypeProps {
+  title: string
+}
+const props = defineProps<TypeProps>()
+</script>
