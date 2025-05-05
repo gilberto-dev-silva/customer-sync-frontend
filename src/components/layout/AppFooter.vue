6674232e662551189bf3c489ff5234b429b471c1
Gilberto Silva <gilbertdsbatista@gmail.com>
2025-05-05 08:13:04 -0300

    feat: add AppFooter component with dynamic copyright

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+      <div class="footer-copyright">
+        <p>&copy; {{ currentYear }} CustomerSync. Todos os direitos reservados.</p>
+      </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+const currentYear = new Date().getFullYear();
+</script>
